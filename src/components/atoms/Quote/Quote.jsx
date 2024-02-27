d1b2c685763cc66c7bce796fcf2a11e96a24ead9
ArcanumLibella <fiona.roux@hetic.net>
2024-02-27 19:17:52 +0100

    feat: add Quote component

--- src/components/atoms/Quote/Quote.jsx
+++ src/components/atoms/Quote/Quote.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 import { Text } from "../Text";
 
-export const Quote = ({ block }) => {
-  console.log("block : ", block);
-
-  function displayQuoteContent(block) {
-    console.log("BLOCK ? -> ", block)
-    if (block /* && block.innerblocks.lenght > 0 */) {
-      console.log("POUET ?")
-      return block.innerblocks.map((innerblock) => {
-        console.log("innerblock ", innerblock);
-        return innerblock.attributes.content;
-      })
-    }
-  }
-
+export const Quote = ({ content }) => {
   return (
-    <div className="p-6 rounded bg-purple-light">
+    <div className="p-6 w-full max-w-full rounded bg-purple-light">
       <Text type="paragraphLarge" className="text-white">
-        Je suis une citation
-        {displayQuoteContent(block)}
+        {content}
       </Text>
     </div>
   )
