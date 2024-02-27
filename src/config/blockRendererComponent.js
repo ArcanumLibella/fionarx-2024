47c60041c0dc37952251dc1d5109e47bad222bdf
ArcanumLibella <fiona.roux@hetic.net>
2024-02-27 19:52:49 +0100

    feat: add dynamic  ListItem component

--- src/config/blockRendererComponent.js
+++ src/config/blockRendererComponent.js
@@ -3,6 +3,7 @@ import { Separator } from "../components/atoms/Separator";
 import { Button } from "../components/molecules/Button";
 import { Quote } from "../components/atoms/Quote";
 import { ButtonLarge } from "../components/molecules/ButtonLarge";
+import { ListItem } from "../components/molecules/ListItem";
 
 export const blockRendererComponent = (block) => {  
   switch (block.name) {
@@ -26,6 +27,15 @@ export const blockRendererComponent = (block) => {
       return (
         <Quote 
           key={block.id}
+          content={block.attributes.data.content}
+        />
+      )
+    }
+    case "frx/listitem": {
+      return (
+        <ListItem 
+          key={block.id}
+          index={block.attributes.data.index}
           content={block.attributes.data.content}
         />
       )
