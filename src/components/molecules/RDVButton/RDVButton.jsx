5553f22bb7c97b70c04a3bb0235c21857c2da4cb
ArcanumLibella <fiona.roux@hetic.net>
2024-03-11 11:21:05 +0100

    feat: Add CTA to RDVButton

--- src/components/molecules/RDVButton/RDVButton.jsx
+++ src/components/molecules/RDVButton/RDVButton.jsx
@@ -3,9 +3,12 @@ import { CalendarIcon } from "../../../assets/icons";
 import { Text } from "../../atoms/Text";
 
 export const RDVButton = () => {
-  // TODO: A transformer en button avec CTA
+  function handleCalendar () {
+    console.log("Prendre RDV !")
+  }
+
   return (
-    <div className="RDVButton fixed top-4 right-4 md:top-8 md:right-8 lg:top-10 lg:right-10 w-16 h-16 z-100 cursor-pointer">
+    <div onClick={handleCalendar} className="RDVButton fixed top-4 right-4 md:top-8 md:right-8 lg:top-10 lg:right-10 w-16 h-16 z-100 cursor-pointer">
       <div className="flex flex-col justify-center items-center w-full h-full animate-transform bg-midnight lg:bg-twilight">
         <Text type="paragraph" className="!font-black text-tiny">
           RDV
